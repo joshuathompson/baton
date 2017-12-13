159b1e644ce3181c9e2df00ac80642d90438ddad
Joshua Thompson <Jethom18@gmail.com>
2017-12-13 15:53:39 -0500

    Remove unused struct

--- api/playlist.go
+++ api/playlist.go
@@ -5,22 +5,6 @@ type PlaylistTrackLinks struct {
 	Total int    `json:"total"`
 }
 
-type FullPlaylist struct {
-	Collaborative bool              `json:"collaborative"`
-	Description   string            `json:"description"`
-	ExternalUrls  map[string]string `json:"external_urls"`
-	Href          string            `json:"href"`
-	ID            string            `json:"id"`
-	Images        []Image           `json:"images"`
-	Name          string            `json:"name"`
-	Owner         *User             `json:"owner"`
-	Public        bool              `json:"public"`
-	SnapshotID    string            `json:"snapshot_id"`
-	Tracks        []FullTracksPaged `json:"tracks"`
-	Type          string            `json:"type"`
-	URI           string            `json:"uri"`
-}
-
 type SimplePlaylist struct {
 	Collaborative bool                `json:"collaborative"`
 	ExternalUrls  map[string]string   `json:"external_urls"`
